eacb042781cb79f6d63d51f3cca6d98d76fede92
Nate <7nater.nelson@gmail.com>
2021-03-22 12:34:09 -0600

    commit tree b1a0e4bc0b25d462a62d582dfa21d83f904adedc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   
+    belongs_to :user
+
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  validates :name, presence: true, uniqueness: true
+  validates :content, presence: true, uniqueness: true
 end
